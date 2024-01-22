e8df54127db942035bdac4ba3c3074fb6c6cbfc9
nylespham <nylespham@gmail.com>
2024-01-22 16:09:02 +0700

    pipeline

--- pipelines/web-agent.groovy
+++ pipelines/web-agent.groovy
@@ -14,7 +14,7 @@ pipeline {
         }
         stage("Discard Old Container"){
             steps {
-                sh "docker rm -f \$(docker ps -qaf 'name=${SERVICE}')"
+                sh "sudo docker rm -f \$(docker ps -qaf 'name=${SERVICE}')"
             }
         }
         stage("Run Container"){
