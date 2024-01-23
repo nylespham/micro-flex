5955df6b98685eae94170d50dd13bef890855871
nylespham <nylespham@gmail.com>
2024-01-23 15:27:39 +0700

    login

--- login-oauth/cmd/api/main.go
+++ login-oauth/cmd/api/main.go
@@ -66,7 +66,7 @@ func openDB(dsn string) (*sql.DB, error) {
 }
 
 func connectToDB() *sql.DB {
-	dsn := "port=5432 dbname=postgres user=postgres password=P@ssw0rd sslmode=disable timezone=UTC connect_timeout=5"
+	dsn := "host=0.0.0.0 port=5432 dbname=postgres user=postgres password=P@ssw0rd sslmode=disable timezone=UTC connect_timeout=5"
 
 	for {
 
