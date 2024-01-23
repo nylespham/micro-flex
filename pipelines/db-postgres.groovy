99556e5585d3afbaed44e407e42e9e20cac6885c
nylespham <nylespham@gmail.com>
2024-01-23 16:57:49 +0700

    docker-compose

--- pipelines/db-postgres.groovy
+++ pipelines/db-postgres.groovy
@@ -1,33 +1,17 @@
 pipeline {
     environment {
-        SERVICE="web-agent"
-        FOLDER="./cmd/web"
-        PORT="8100"
+        SERVICE="db-postgres"
     }
     agent {
         label "jenkins-02"
     }
-    stages {
-        stage("Build Image"){
-            steps {
-                sh "sudo docker build -t ${SERVICE}:latest -f dockerfiles/Dockerfile . --build-arg SERVICE_NAME=${SERVICE} --build-arg FOLDER=${FOLDER}"
-            }
-        }
-        stage("Discard Old Container"){
-            steps {
-                dir ("./compose") {
-                    sh "sudo docker compose down ${SERVICE}"
-                }
-            }
-        }
-        stage("Run Container"){
+    stage("Run Container"){ 
             steps {
                 dir("./compose") {
                     sh "sudo docker compose up -d ${SERVICE}"
                 }
             }
         }
-    }
     post {
         always {
             cleanWs(cleanWhenNotBuilt: false,
